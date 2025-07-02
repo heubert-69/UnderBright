f45cc95234968827937cca385ad0a5c9860b3400
John Michael Coronel <105194968+heubert-69@users.noreply.github.com>
2025-07-02 19:58:33 +0800

    Fixing EnemyAI.cs

--- prototype/Assets/EnemyAI.cs
+++ prototype/Assets/EnemyAI.cs
@@ -9,46 +9,42 @@ public class EnemyAI : MonoBehaviour
 {
     //Reference to the Waypoints
     public List<Transform> points;
-
     public int nextId = 0;
     //Default changing ID value for The enemy id
     public int changeValueID = 1;
     //The Speed Where the enemy is moving
     public int speed = 2;
 
-
-
     private void Reset()
     {
         Init();
     }
 
-
     public void Init()
     {
         //When Enemy Hits an object
-        GetComponent<BoxCollider2D>().isTrigger() = true;
-
+        GetComponent<BoxCollider2D>().isTrigger = true;
 
         //Setting The Enemy Root Object (Position)
         GameObject root = new GameObject(name + "_Root");
         //Reset the position of the enemy
         root.transform.position = transform.position;
         //This sets the enemy child node
-        transform.SetParent();
+        transform.SetParent(root.transform);
         //This Create The Waypoints between the 2 platform
-        GameObject waypoints = new GameObejct("Waypoint");
+        GameObject waypoints = new GameObject("Waypoints");
         //Creates The enemy object
         waypoints.transform.SetParent(root.transform);
         waypoints.transform.position = root.transform.position;
         //Make Waypoints of the child nodes(Enemy)
         //Basically This sets the global position of the boss
-        GameObject p1 = new GameObject("Point1"); p1.transform.SetParent(waypoints.transform); p1.transform.position = root.transform.position;
-        GameObject p2 = new GameObject("Point2"); p2.transform.SetParent(waypoints.transform); p2.transform.position = root.transform.position;
+        GameObject p1 = new GameObject("Point1"); p1.transform.SetParent(waypoints.transform); p1.transform.position = Vector3.right * 2 + root.transform.position;
+        GameObject p2 = new GameObject("Point2"); p2.transform.SetParent(waypoints.transform); p2.transform.position = Vector3.left * 2 + root.transform.position;
 
         //Initialize The Points List
-        points = List<Transform>();
+        points = new List<Transform>();
         points.Add(p1.transform);
+        points.Add(p2.transform);
     }
 
     private void Update()
@@ -56,13 +52,15 @@ public class EnemyAI : MonoBehaviour
         MoveToNextPoint();
     }
 
-    //We're making the enemy patrol system
     public void MoveToNextPoint()
     {
-        //Basically, This gets the enemy ai to the next point
-        //Then, We Flip The Enemy Character (With Animation) to the Golden Point or P2
+        if (points == null || points.Count == 0)
+            return;
+
+        //Get the next waypoint
         Transform goalPoint = points[nextId];
-        //the next point of the enemy ai
+        
+        //Flip the enemy based on direction
         if (goalPoint.transform.position.x > transform.position.x)
         {
             transform.localScale = new Vector3(-1, 1, 1);
@@ -71,25 +69,25 @@ public class EnemyAI : MonoBehaviour
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        //Moving The Enemy Towards The Goal Point
+        
+        //Move towards the goal point
         transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed * Time.deltaTime);
-        //Checking The Distance Between The 2 Points of The AI (To Trigger The Next Point)
+        
+        //Check if we've reached the current waypoint
         if (Vector2.Distance(transform.position, goalPoint.position) < 0.5f)
         {
-            //This Checks if we are at the end of the line (position -= 1)
-            //Also Checks if we are at the next point (position += 1)
-            //After The Condition Checking we Apply changes to the 'nextID' variable
+            //Check if we're at the end of the path
             if (nextId == points.Count - 1)
             {
                 changeValueID = -1;
             }
-            if (nextId == 0)
+            //Check if we're at the start of the path
+            else if (nextId == 0)
             {
                 changeValueID = 1;
-            } nextId += changeValueID;
-
+            }
+            
+            nextId += changeValueID;
         }
-
     }
-
 }
